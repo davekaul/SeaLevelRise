709509393dfb59520ba5c3f8aeb8fc9c5b7ad5f5
Dave Kaul <davekaul@gmail.com>
2022-09-18 12:05:32 -0400

    Create foundation for QuizBuilder

---
+++ Assets/Scripts/QuizBuilder.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizBuilder : MonoBehaviour
+{
+    [SerializeField] private GameObject _quizPrefab;
+    [SerializeField] private ScriptableObject _quizData;
+}
